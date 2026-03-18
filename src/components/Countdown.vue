fb78f1aef1efd229af45a9f56b5103d75fe98ae9
Loris Witschard <loris.wit@gmail.com>
2026-03-18 23:44:01 +0100

    Redesign pour RastaLAN 8

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -14,7 +14,15 @@ onMounted(() => {
   window.onfocus = updateSecondsLeft
 })
 
-setInterval(() => --secondsLeft.value, 1000)
+setInterval(() => {
+  if (secondsLeft.value > 0) {
+    --secondsLeft.value
+  }
+  if (secondsLeft.value <= 0) {
+    secondsLeft.value = 0
+    location.reload()
+  }
+}, 1000)
 
 const countdown = computed(() => {
   const days = Math.floor(secondsLeft.value / 3600 / 24)
@@ -27,10 +35,84 @@ const countdown = computed(() => {
   const sec = Math.floor(secondsLeft.value % 60)
     .toString()
     .padStart(2, "0")
-  return `${days}:${hours}:${min}:${sec}`
+
+  return { days, hours, min, sec }
 })
 </script>
 
 <template>
-  <span class="countdown">{{ countdown }}</span>
+  <div class="countdown">
+    <div>
+      <span class="value">{{ countdown.days }}</span>
+      <span class="label">jours</span>
+    </div>
+    <div>
+      <span class="value">{{ countdown.hours }}</span>
+      <span class="label">heures</span>
+    </div>
+    <div>
+      <span class="value">{{ countdown.min }}</span>
+      <span class="label">min</span>
+    </div>
+    <div>
+      <span class="value">{{ countdown.sec }}</span>
+      <span class="label">sec</span>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.countdown {
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+  margin: 1rem;
+
+  > * {
+    transform: skew(-10deg);
+    display: flex;
+    flex-direction: column;
+    background-color: #4004;
+    border-radius: 0.2em;
+    padding: 0.2em 0.4em;
+
+    .label {
+      font-size: 0.3em;
+      opacity: 75%;
+      text-transform: uppercase;
+    }
+
+    &:nth-child(1) {
+      --delay: 0.7s;
+    }
+
+    &:nth-child(2) {
+      --delay: 0.8s;
+    }
+
+    &:nth-child(3) {
+      --delay: 0.9s;
+    }
+
+    &:nth-child(4) {
+      --delay: 1s;
+    }
+  }
+}
+
+.animated .countdown > * {
+  animation: 1.2s var(--delay) cubic-bezier(0.32, 1.01, 0.26, 1.17) both slide-up;
+}
+
+@keyframes slide-up {
+  from {
+    translate: 0 100px;
+    opacity: 0;
+  }
+
+  to {
+    translate: 0 0;
+    opacity: 100%;
+  }
+}
+</style>
